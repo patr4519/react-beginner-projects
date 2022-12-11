8f13bd10425ed175b43013dca4918d508937dc0d
patr4519 <jero96@mail.ru>
2022-12-11 22:39:36 +0300

    Start version

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import './index.scss';
+import { Success } from './components/Success';
+import { Users } from './components/Users';
+
+// Тут список пользователей: https://reqres.in/api/users lsdfl
 
 function App() {
-  const [open, setOpen] = React.useState(false);
-
   return (
     <div className="App">
-      <button onClick={() => setOpen(true)} className="open-modal-btn">✨ Открыть окно</button>
-      <div onClick={() => setOpen(false)} className={`overlay animated ${open ? 'show' : ''}`}>
-        <div className="modal">
-          <svg height="200" viewBox="0 0 200 200" width="200">
-            <title />
-            <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
-          </svg>
-          <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
-        </div>
-      </div>
+      <Users />
+      {/* <Success /> */}
     </div>
   );
 }
